e88d7329b69b02c324d6fa4a24508094e41ee8cd
udult <dmspiridonoff02@yandex.ru>
2025-10-17 13:17:56 +0700

    DH_KE

---
+++ src/client.cpp
@@ -0,0 +1,168 @@
+// client side
+#include <stdio.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#include <string>
+#include <iostream>
+
+#include <cryptopp/dh.h>
+#include <cryptopp/hex.h>
+#include <cryptopp/osrng.h>
+
+#define PORT 2001
+#define IP "127.0.0.1"
+
+#define CAPACITY 2048
+
+using namespace CryptoPP;
+
+int main()
+{
+	// client init
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1)
+	{
+		perror("socket");
+		return 1;
+	}
+
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(PORT);
+	addr.sin_addr.s_addr = inet_addr(IP);
+
+	if (connect(sockfd, (struct sockaddr*) &addr, sizeof(addr)) == -1)
+	{
+		perror("connect");
+		shutdown(sockfd, SHUT_RDWR);
+		close(sockfd);
+		return 1;
+	}
+
+	std::cout << "Client is connected on " << inet_ntoa(addr.sin_addr) << ':' << ntohs(addr.sin_port) << std::endl;
+
+	// receive dh common
+	char buffer[CAPACITY] = {'\0'};
+
+	if (recv(sockfd, buffer, sizeof(buffer), 0) <= 0)
+	{
+		std::cout << "Failed to receive DH common" << std::endl;
+		shutdown(sockfd, SHUT_RDWR);
+		close(sockfd);
+		return 1;
+	}
+
+	// unpack dh common
+	std::string p_s, q_s, g_s;
+
+	int count = 0;
+
+	while (buffer[count] != '.' && buffer[count] != '\0')
+		p_s.push_back(buffer[count++]);
+
+	count++;
+
+	while (buffer[count] != '.' && buffer[count] != '\0')
+		q_s.push_back(buffer[count++]);
+
+	count++;
+
+	while (buffer[count] != '.' && buffer[count] != '\0')
+		g_s.push_back(buffer[count++]);
+
+	std::cout << "\nDH common parameters received:\n" << "p: " << p_s << '\n' << "q: " << q_s << '\n' << "g: " << g_s << std::endl;
+	
+	try
+	{
+		// init dh common
+		DH dh;
+		AutoSeededRandomPool prng;
+
+		const Integer p(p_s.c_str());
+		const Integer q(q_s.c_str());
+		const Integer g(g_s.c_str());
+
+		dh.AccessGroupParameters().Initialize(p, q, g);
+
+		// validate dh common
+		if (!dh.GetGroupParameters().ValidateGroup(prng, 3))
+			throw std::runtime_error("dh: Failed to validate prime and generator");
+
+		// gen dh key pair
+		SecByteBlock pub(dh.PublicKeyLength());
+		SecByteBlock priv(dh.PrivateKeyLength());
+
+		dh.GenerateKeyPair(prng, priv, pub);
+
+		std::string pub_s; 
+		std::string priv_s;
+
+		ArraySource(pub.data(), pub.size(), true,
+			new HexEncoder(
+				new StringSink(pub_s)
+			)
+		);
+
+		ArraySource(priv.data(), priv.size(), true,
+			new HexEncoder(
+				new StringSink(priv_s)
+			)
+		);
+
+		std::cout << "\nDH public key generated:\n" << pub_s << std::endl;
+		std::cout << "\nDH private key generated:\n" << priv_s << std::endl;
+
+		// send dh public key
+		if (send(sockfd, pub.data(), pub.size(), 0) <= 0)
+			std::runtime_error("Failed to send DH public key");
+
+		// receive dh public key
+		SecByteBlock pubRecv(dh.PublicKeyLength());
+
+		if (recv(sockfd, pubRecv.data(), pubRecv.size(), 0) <= 0)
+			std::runtime_error("Failed to receive DH public key");
+
+		std::string pubRecv_s;
+
+		ArraySource(pubRecv.data(), pubRecv.size(), true,
+			new HexEncoder(
+				new StringSink(pubRecv_s)
+			)
+		);
+
+		std::cout << "\nDH public key received:\n" << pubRecv_s << std::endl;
+
+		// calc secret key
+		SecByteBlock secretKey(dh.AgreedValueLength());
+		
+		if (!dh.Agree(secretKey, priv, pubRecv))
+			throw std::runtime_error("dh: Failed to calculate secret key");
+
+		std::string secretKey_s;
+
+		ArraySource(secretKey.data(), secretKey.size(), true,
+			new HexEncoder(
+				new StringSink(secretKey_s)
+			)
+		);
+
+		std::cout << "\nDH secret key calculated:\n" << secretKey_s << std::endl;
+	}
+
+	catch (const Exception &exc)
+	{
+		std::cout << exc.what() << std::endl;
+		shutdown(sockfd, SHUT_RDWR);
+		close(sockfd);
+		return 1;
+	}
+
+	shutdown(sockfd, SHUT_RDWR);
+	close(sockfd);
+
+	return 0;
+}
+
+/* _EOF_ */
---
+++ src/server.cpp
@@ -0,0 +1,165 @@
+// server side
+#include <stdio.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#include <string>
+#include <sstream>
+#include <iostream>
+
+#include <cryptopp/dh.h>
+#include <cryptopp/hex.h>
+#include <cryptopp/osrng.h>
+
+#define PORT 2001
+#define IP "127.0.0.1"
+
+#define CAPACITY 2048 
+
+using namespace CryptoPP;
+
+int main()
+{
+	/********** server init **********/
+	int server = socket(AF_INET, SOCK_STREAM, 0);
+	if (server == -1)
+	{
+		perror("socket");
+		return 1;
+	}
+
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(PORT);
+	addr.sin_addr.s_addr = inet_addr(IP);
+
+	if (bind(server, (struct sockaddr*) &addr, sizeof(addr)) == -1)
+	{
+		perror("bind");
+		shutdown(server, SHUT_RDWR);
+		close(server);
+		return 1;
+	}
+
+	std::cout << "Server is running at " << inet_ntoa(addr.sin_addr) << ':' << ntohs(addr.sin_port) << std::endl;
+
+	if (listen(server, 2) == -1)
+	{
+		perror("listen");
+		shutdown(server, SHUT_RDWR);
+		close(server);
+		return 1;
+	}
+
+	std::cout << "Waiting for connection..." << std::endl;
+
+	/********** accept connection **********/
+	int client1, client2;
+
+	struct sockaddr_in clientaddr1, clientaddr2;
+
+	socklen_t len1, len2;
+	len1 = len2 = sizeof(sockaddr_in);
+
+	client1 = accept(server, (struct sockaddr*) &clientaddr1, &len1);
+	if (client1 == -1)
+	{
+		perror("accept");
+		shutdown(server, SHUT_RDWR);
+		close(server);
+		return 1;
+	}
+
+	std::cout << "Client 1 is connected from " << inet_ntoa(clientaddr1.sin_addr) << ':' << ntohs(clientaddr1.sin_port) << std::endl;
+
+	client2 = accept(server, (struct sockaddr*) &clientaddr2, &len2);
+	if (client2 == -1)
+	{
+		perror("accept");
+		shutdown(server, SHUT_RDWR);
+		close(server);
+		return 1;
+	}
+
+	std::cout << "Client 2 is connected from " << inet_ntoa(clientaddr2.sin_addr) << ':' << ntohs(clientaddr2.sin_port) << std::endl;
+
+	/********** Diffie-Hellman key exchange **********/
+	try
+	{
+		// gen dh common
+		DH dh;
+		AutoSeededRandomPool prng;
+		dh.AccessGroupParameters().GenerateRandomWithKeySize(prng, CAPACITY);
+
+		const Integer &p = dh.GetGroupParameters().GetModulus();
+		const Integer &q = dh.GetGroupParameters().GetSubgroupOrder();
+		const Integer &g = dh.GetGroupParameters().GetGenerator();
+
+		std::cout << "\nDH common parameters:\n" << "p: " << p << '\n' << "q: " << q << '\n' << "g: " << g << std::endl;
+		
+		// send dh common
+		std::stringstream ss;
+		ss << p << q << g;
+
+		std::string buffer = ss.str();
+
+		if (send(client1, buffer.c_str(), buffer.size(), 0) <= 0)
+			std::runtime_error("Failed to send DH common to client 1");
+
+		if (send(client2, buffer.c_str(), buffer.size(), 0) <= 0)
+			std::runtime_error("Failed to send DH common to client 2");
+
+		// receive public dh keys from clients
+		SecByteBlock publicKey1(dh.PublicKeyLength());
+		SecByteBlock publicKey2(dh.PublicKeyLength());
+
+		if (recv(client1, publicKey1.data(), publicKey1.size(), 0) <= 0)
+			std::runtime_error("Failed to receive DH public key from client 1");
+
+		if (recv(client2, publicKey2.data(), publicKey2.size(), 0) <= 0)
+			std::runtime_error("Failed to receive DH public key from client 2");
+
+		std::string pub1_s;
+		std::string pub2_s;
+
+		ArraySource(publicKey1.data(), publicKey1.size(), true,
+			new HexEncoder(
+				new StringSink(pub1_s)
+			)
+		);
+
+		ArraySource(publicKey2.data(), publicKey2.size(), true,
+			new HexEncoder(
+				new StringSink(pub2_s)
+			)
+		);
+
+		std::cout << "\nClient 1 DH public key:\n" << pub1_s << std::endl;
+		std::cout << "\nClient 2 DH public key:\n" << pub2_s << std::endl;
+
+		// resend public dh keys between clients
+		if (send(client1, publicKey2.data(), publicKey2.size(), 0) <= 0)
+			std::runtime_error("Failed to resend DH public key to client 1");
+
+		if (send(client2, publicKey1.data(), publicKey1.size(), 0) <= 0)	
+			std::runtime_error("Failed to resend DH public key to client 2");
+
+		std::cout << "\nDH public keys has been exchanged" << std::endl;
+	}
+
+	catch (const Exception &exc)
+	{
+		std::cout << exc.what() << std::endl;
+		shutdown(server, SHUT_RDWR);
+		close(server);
+		return 1;
+	}
+
+	shutdown(server, SHUT_RDWR);
+	close(server);
+
+	return 0;
+}
+
+/* _EOF_ */
